c855131c6825d11c6151ee9db31fed7bb5651296
Mrjwj34 <jwj3050667529@gmail.com>
2025-09-22 15:45:17 +0800

    first commit

---
+++ backend/brilliant-tavern/src/main/java/com/github/jwj/brilliantavern/BrilliantTavernApplication.java
@@ -0,0 +1,13 @@
+package com.github.jwj.brilliantavern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrilliantTavernApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrilliantTavernApplication.class, args);
+    }
+
+}
